9d0742844e544bbb8ca61f11eddc9a05ef488111
adesob <55300413+adesob@users.noreply.github.com>
2021-06-23 14:13:34 -0300

    .

--- projects.js
+++ projects.js
@@ -1,6 +1,7 @@
 const home = document.querySelector(".homeContainer");
-// const imgBx = document.querySelector()
+const projLink = document.querySelector("#projects-av")
 const homeChildren = Array.from(home.children);
+
 var proj = ["LessTrvld", "Emerge", "ExpressCare", "Adedotcom"]
 var descContent =  ["Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus odio ex eligendi! Pariatur, debitis quaerat!",
 "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus odio ex eligendi! Pariatur, debitis quaerat!",
@@ -8,7 +9,9 @@ var descContent =  ["Lorem ipsum dolor sit, amet consectetur adipisicing elit. D
 "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus odio ex eligendi! Pariatur, debitis quaerat!"]
 var techUsedContent = ["JavaScript • React","Java • MicroPython • Android Studio • SolidWorks", "Java • Android Studio • Firebase  ", "HTML • CSS • JavaScript"]
 var pics = ["lessTrvld.jpg", "emerge.jpg","ExpressCare.jpg","adedotcom.jpg"]
+var links = ["","","",""]
 
+// const imgBx = document.querySelector()
 // var lessTrvldTech = ["fab fa-js-square","fab fa-react"]
 // var emergeTech = ["fab fa-java","fab fa-android"]
 // var expressTech = ["fab fa-java","fab fa-android"]
@@ -16,7 +19,7 @@ var pics = ["lessTrvld.jpg", "emerge.jpg","ExpressCare.jpg","adedotcom.jpg"]
 
 
 
-home.addEventListener('click', e =>{
+projLink.addEventListener('click', e =>{
     homeChildren.forEach(removeElement)
     createProjects()
     
@@ -27,18 +30,19 @@ function removeElement(element){
 }
 
 function insertContent(num){
-    const image = document.createElement("img");
-    image.id = "pic" + num
-    image.className = "pic"
-    image.src = pics[num-1]
+    // const image = document.createElement("img");
+    // image.id = "pic" + num
+    // image.className = "pic"
+    // image.src = pics[num-1]
 
-    const imageBx = document.querySelector("#imageBx" + num)
-    imageBx.append(image)
+    // const imageBx = document.querySelector("#imageBx" + num)
+    // imageBx.append(image)
     
     const desc = document.querySelector("#description" + num)
 
     const heading = document.createElement("p")
     const techUsed = document.createElement("p")
+    const link = document.createElement("a")
 
     heading.id = "explan" + num
     heading.className = "explan"
@@ -47,8 +51,14 @@ function insertContent(num){
     techUsed.id = "techUsed" + num
     techUsed.innerText = techUsedContent[num-1]
 
+    // link.className = "link"
+    link.href = links[num-1]
+    link.className = "gitLink"
+    link.innerHTML = '<i class="fa fa-external-link" id="link" aria-hidden="true"></i>'
+
     desc.append(heading)
     desc.append(techUsed)
+    desc.append(link)
 
     desc.firstElementChild.innerHTML = descContent[num-1]
     
@@ -59,6 +69,46 @@ function insertContent(num){
     // else if (num == 3){ express(num) }
     // else { adedotcom(num) }
 }
+
+
+function createProjects(){
+    home.className = "projectsContainer";
+
+    for( var i = 0; i<4; i++){
+        const item = document.createElement("div")
+        item.id = "item" + (i+1)
+        item.className = "items"
+        home.append(item)
+
+        item.append(document.createElement("div"))
+        item.append(document.createElement("h1"))
+
+        const itemsContent = Array.from(item.children)
+
+        child = document.createElement("div")
+        child.id = "description" + (i+1)
+
+        const image = document.createElement("img");
+        image.id = "pic" + (i+1)
+        image.className = "pic"
+        image.src = pics[i]
+
+        // const imageBx = document.querySelector("#imageBx" + num)
+        // imageBx.append(image)
+
+        itemsContent[0].id = "imageBx" + (i+1)
+        itemsContent[0].append(image)
+        itemsContent[0].append(child)
+
+        itemsContent[1].id = "projectName" + (i+1)
+        itemsContent[1].innerText = proj[i]
+
+        insertContent(i+1);
+    }
+}
+
+
+
 
 
 function lessTrvld(num){
@@ -101,33 +151,3 @@ function adedotcom(num){
         x.append(av);
     }
 }
-
-
-
-function createProjects(){
-    home.className = "projectsContainer";
-
-    for( var i = 0; i<4; i++){
-        const item = document.createElement("div")
-        item.id = "item" + (i+1)
-        item.className = "items"
-        home.append(item)
-
-        item.append(document.createElement("div"))
-        item.append(document.createElement("h1"))
-
-        const itemsContent = Array.from(item.children)
-
-        child = document.createElement("div")
-        child.id = "description" + (i+1)
-
-        itemsContent[0].id = "imageBx" + (i+1)
-        itemsContent[0].append(child)
-
-        itemsContent[1].id = "projectName" + (i+1)
-        itemsContent[1].innerText = proj[i]
-
-        insertContent(i+1);
-    }
-}
-
